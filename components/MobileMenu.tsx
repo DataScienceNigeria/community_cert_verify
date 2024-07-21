1356b493c36cc365612bdf918dea012f34c9f934
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-21 21:48:33 +0100

    test

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -14,11 +14,19 @@ import { TiThMenu } from "react-icons/ti";
 import Link from 'next/link';
 import { sideNav } from "@/components/SideBar"
 import { usePathname } from 'next/navigation';
+import { signOut } from 'next-auth/react';
+
 
 
 export function MobileMenu() {
 
-    const pathname: string = usePathname();
+  const pathname: string = usePathname();
+
+  const handleSignOut = (e: any) => {
+    e.preventDefault();
+    signOut({ callbackUrl: '/login',  redirect:true  });
+}
+  
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -28,7 +36,7 @@ export function MobileMenu() {
       {
             sideNav.map((item, index) => (
                 <div key={index} className={`w-[100%] flex items-center justify-center  p-4 hover:border-b-2 cursor-pointer transition-all ${pathname === item.path ? 'bg-green-500 text-white' : 'bg-white'} font-semibold`}>       
-                    <Link href={item.path} className='flex items-center gap-x-4 justify-between'>
+                    <Link href={item.path} className='flex items-center gap-x-4 justify-between' onClick={item.title === 'Sign Out' ? handleSignOut : undefined}>
                         <item.icon />
                         <span>{item.title}</span>
                     </Link>
