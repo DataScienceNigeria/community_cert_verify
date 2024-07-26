1cd5f52b87f4282f2dc9b82f1b36e88a92a52dc7
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-26 14:01:13 +0100

    test

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { TiThMenu } from "react-icons/ti"; 
 import {MobileMenu} from './MobileMenu';
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
     <nav className='h-10 flex justify-between items-center my-4 px-4 md:px-0'>
       <div>
-        <h1 className='text-sm md:text-2xl font-extrabold text-green-dsn bg-white p-2 rounded-xl'>DSN Community Certificate Issuer</h1>
+        <Link href="/dashboard" className='text-sm md:text-2xl font-extrabold text-green-dsn bg-white p-2 rounded-xl'>DSN Community Certificate Issuer</Link>
       </div>
         <div className='cursor-pointer z-30 md:hidden'>
           <MobileMenu />
