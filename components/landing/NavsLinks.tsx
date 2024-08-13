94e332599891cf98290155df04b67594bad22a17
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-08-13 20:19:48 +0100

    test

--- components/landing/NavsLinks.tsx
+++ components/landing/NavsLinks.tsx
@@ -18,7 +18,7 @@ const NavsLinks = () => {
             <ul className='flex gap-x-4 text-sm text-black/50'>
                 {HomeNavsLinks.map((nav, index) => (
                 <li key={index} className={`cursor-pointer transition-all py-2 ${pathname === nav.path ? 'bg-green-dsn text-white' : ''} font-semibold py-1`}>
-                    <Link href={nav.path} className={`${nav.title === "Login In"? "border-green-dsn border": ""} px-8 py-2`}>{nav.title}</Link>
+                    <Link href={nav.path} className={`${nav.title === "Verify"? "border-green-dsn border": ""} px-8 py-2`}>{nav.title}</Link>
                 </li>
                 ))}
             </ul>
